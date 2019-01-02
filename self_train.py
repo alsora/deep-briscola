e79fadd11daf10a9d1de861e2b892339ebc3657d
alsora <alberto.soragna@gmail.com>
2019-01-02 20:36:47 +0100

    updated self_train.py and graphic_visualizations.py to latest commit

--- self_train.py
+++ self_train.py
@@ -1,10 +1,13 @@
 import tensorflow as tf
-import environment as brisc
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import copy
+
+## our stuff import
+import graphic_visualizations as gv
+import environment as brisc
+from graphic_visualizations import stats_plotter
 
 from agents.random_agent import RandomAgent
 from agents.q_agent import QAgent
@@ -23,6 +26,7 @@ def play_episode(game, agents):
 
             player = game.players[player_id]
             agent = agents[player_id]
+
             # agent observes state before acting
             agent.observe(game, player, game.deck)
             available_actions = game.get_player_actions(player_id)
@@ -89,7 +93,7 @@ def evaluate(game, agents, num_evaluations):
 
 
 
-def self_train(game, agent, num_epochs, evaluate_every, num_evaluations, model_dir = "", evaluation_dir = "evaluation_dir"):
+def self_train(game, agent, num_epochs, evaluate_every, num_evaluations, random_every, model_dir = "", evaluation_dir = "evaluation_dir"):
 
     # Initializing the list of agents with the agent and a copy of him
     if not os.path.isdir('cur_model_copy'):
@@ -99,24 +103,27 @@ def self_train(game, agent, num_epochs, evaluate_every, num_evaluations, model_d
     first_old.load_model('cur_model_copy')
     old_agents = [agent, first_old]
 
-
-
+    # Training starts
     best_winning_ratio = -1
     for epoch in range(1, num_epochs + 1):
-        print(chr(27) + '[2J')
 
         # picking an agent from the past self
         old_num = np.random.randint(1,len(old_agents))
         agents= [agent, old_agents[len(old_agents)-old_num]]
 
-        print (f"Epoch: {epoch} Agent {old_num} old", end='\r')
 
+        print(chr(27) + '[2J')
+        gv.printProgressBar(epoch, num_epochs,
+                            prefix = f'Agent {old_num} old\nEpoch: {epoch}',
+                            length= 50)
 
-        game_winner_id, winn500er_points = play_episode(game, agents)
+        # During the playing of the episode the agent learn
+        play_episode(game, agents)
 
+        # Evaluation step
         if epoch % evaluate_every == 0:
-            for agent in agents:
-                agent.make_greedy()
+            for ag in agents:
+                ag.make_greedy()
             victory_rates, average_points = evaluate(game, agents, num_evaluations)
             victory_rates_hist.append(victory_rates)
             average_points_hist.append(average_points)
@@ -125,44 +132,46 @@ def self_train(game, agent, num_epochs, evaluate_every, num_evaluations, model_d
             if not os.path.isdir(evaluation_dir):
                 os.mkdir(evaluation_dir)
 
-            df  = np.array(average_points_hist)
-            evaluation_num = len(df[:,0,0])
-            eval_df = np.array(average_points_hist)[evaluation_num-1,:,:]
-            eval_df = pd.DataFrame(eval_df.T, columns = ["Agent 0","Agent 1"])
-
-            eval_df.plot()
-            plt.hlines([np.mean(eval_df.values),
-                        np.mean(eval_df.values)+np.std(eval_df.values),
-                        np.mean(eval_df.values)-np.std(eval_df.values)],
-                       0, len(df[0,0,:]), color = ['green','red','red'],
-                       label = 'mean+-std')
-            plt.title(f"""Eval:{evaluation_num}_
-                      epoch:{evaluation_num*FLAGS.evaluate_every}_
-                      pctWinBest:{max(victory_rates_hist[-1])}_
-                      std:{np.std(eval_df.values).round(2)}""".replace('\n','').replace(' ',''))
-            plt.ylim(0,120)
-            plt.xlabel("Evaluation step")
-            plt.ylabel("Points")
-            plt.legend()
-            plt.savefig(f"{evaluation_dir}/fig_{epoch}")
-            plt.close()
-
+            std_cur = gv.eval_visua_for_self_play(average_points_hist,
+                             FLAGS,
+                             victory_rates_hist,
+                             evaluation_dir,
+                             epoch)
             # Storing std
-            std_hist.append(np.std(eval_df.values).round(2))
+            std_hist.append(std_cur)
 
 
-            for agent in agents:
-                agent.restore_epsilon()
-            #print("DeepAgent wins ", "{:.2f}".format(victory_rates[0]), "% with average points ", "{:.2f}".format(average_points[0]))
-            if victory_rates[0] > best_winning_ratio:
-                best_winning_ratio = victory_rates[0]
-                #agents[0].save_model(model_dir)
+            for ag in agents:
+                ag.restore_epsilon()
+
+
 
             # After the evaluation we add the agent to the old agents
             agent.save_model('cur_model_copy')
             new_old_agent = QAgent()
             new_old_agent.load_model('cur_model_copy')
             old_agents.append(new_old_agent)
+
+            # Eliminating the oldest agent if maximum number of agents
+            if len(old_agents) > FLAGS.max_old_agents:
+                old_agents.pop(0)
+
+        # Evaluation against random agent
+        if epoch % random_every == 0:
+            agents = [agent, RandomAgent()]
+            for ag in agents:
+                ag.make_greedy()
+
+            winners, points = evaluate(game, agents, FLAGS.num_evaluations)
+            stats_plotter(agents, points, winners, evaluation_dir,'againstRandom',epoch)
+
+            for ag in agents:
+                ag.restore_epsilon()
+
+            # Saving the model if the agents permorm better against random agent
+            if victory_rates[0] > best_winning_ratio:
+                best_winning_ratio = victory_rates[0]
+                agent.save_model(model_dir)
 
 
     return best_winning_ratio
@@ -174,12 +183,17 @@ def main(argv=None):
 
     global victory_rates_hist
     victory_rates_hist  = []
-
     global average_points_hist
     average_points_hist = []
-
     global std_hist
     std_hist = []
+
+    global victory_rates_hist_against_Random
+    victory_rates_hist_against_Random  = []
+    global average_points_hist_against_Random
+    average_points_hist_against_Random = []
+    global std_hist_against_Random
+    std_hist_against_Random = []
 
     # Initializing the environment
     game = brisc.BriscolaGame(2, verbosity=brisc.LoggerLevels.TRAIN)
@@ -189,33 +203,17 @@ def main(argv=None):
         FLAGS.epsilon, FLAGS.epsilon_increment, FLAGS.epsilon_max, FLAGS.discount,
         FLAGS.learning_rate)
 
-    self_train(game, agent, FLAGS.num_epochs, FLAGS.evaluate_every,
-          FLAGS.num_evaluations, FLAGS.model_dir, FLAGS.evaluation_dir)
-
-
+    # Training
+    best_winning_ratio = self_train(game, agent,
+                                    FLAGS.num_epochs,
+                                    FLAGS.evaluate_every,
+                                    FLAGS.num_evaluations,
+                                    FLAGS.against_random_every,
+                                    FLAGS.model_dir,
+                                    FLAGS.evaluation_dir)
+    print(f'Best winning ratio : {best_winning_ratio}')
     # SUMMARY GRAPH
-    df = np.vstack([np.array(victory_rates_hist).T,np.array(std_hist)]).T
-    vict_rate = pd.DataFrame(df, columns = ["Agent 0 win_rate","Agent 1 win_rate", "Std"])
-
-    vict_rate['Agent 0 win_rate'].plot(secondary_y=False,
-                                       color = 'lightgreen',
-                                       label='Agent 0 (left)')
-    vict_rate['Agent 1 win_rate'].plot(secondary_y=False,
-                                       color = 'lightblue',
-                                       label='Agent 1 (left)')
-    plt.hlines([np.mean(vict_rate.values[:,0]),
-                np.mean(vict_rate.values[:,1])],
-               0, len(vict_rate)-1, color = ['green','blue'],
-               label = 'means')
-    plt.ylabel('WinRate')
-    plt.legend()
-
-    vict_rate.Std.plot(secondary_y=True, label="Std (right)", color = 'red',
-                       alpha = 0.8, linestyle='-.')
-    plt.ylabel('StandardDeviation', rotation=270, labelpad=15)
-    plt.legend()
-    plt.savefig(f"{FLAGS.evaluation_dir}/last")
-
+    gv.summ_vis_self_play(victory_rates_hist, std_hist, FLAGS)
 
 
 
@@ -233,7 +231,8 @@ if __name__ == '__main__':
 
     # Training parameters
     tf.flags.DEFINE_integer("batch_size", 100, "Batch Size")
-    tf.flags.DEFINE_integer("num_epochs", 61, "Number of training epochs")
+    tf.flags.DEFINE_integer("num_epochs", 100000, "Number of training epochs")
+    tf.flags.DEFINE_integer("max_old_agents", 100, "Maximum number of old copies of self stored")
 
     # Deep Agent parameters
     tf.flags.DEFINE_float("epsilon", 0, "How likely is the agent to choose the best reward action over a random one (default: 0)")
@@ -246,8 +245,9 @@ if __name__ == '__main__':
 
 
     # Evaluation parameters
-    tf.flags.DEFINE_integer("evaluate_every", 30, "Evaluate model after this many steps (default: 1000)")
-    tf.flags.DEFINE_integer("num_evaluations", 100, "Evaluate on these many episodes for each test (default: 500)")
+    tf.flags.DEFINE_integer("evaluate_every", 1000, "Evaluate model after this many steps (default: 1000)")
+    tf.flags.DEFINE_integer("against_random_every", 2000, "Evaluate model after this many steps (default: 1000)")
+    tf.flags.DEFINE_integer("num_evaluations", 200, "Evaluate on these many episodes for each test (default: 500)")
 
     FLAGS = tf.flags.FLAGS
 
